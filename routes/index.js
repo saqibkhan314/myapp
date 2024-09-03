71369fbcfbd78bc0a1bf251ec20e950b12a13047
react-native <khansaqibkhan314@gmail.com>
2024-09-03 17:35:23 +0530

    finalmyapp

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,28 @@ var express = require('express');
 var router = express.Router();
 const userModel = require("./users");// what we have exported using mongoose.exports() and users is name of file in which we have written exportd and imported in userModel
 router.get('/',function(req,res){
-  res.send("hello saqib khan")
+ req.session.anyNameCanBeGiven = true;//it will be until unless server is not reloaded means until we run once again npx nodemon
+ res.send("Hello from Saqib khan")
 })
+router.get('/checkban',function(req,res){
+  console.log(req.session);
+  console.log("You are ban");
+  res.send("You are ban")
+  
+  
+})
+router.get('/removeban',function(req,res){
+  req.session.destroy( (err) => {
+      if (err) {
+        throw err;
+      }
+  } )
+  res.send("Ban removed");
+  
+  
+})
+
+
 router.get('/create',async function(req,res){
   //now we have to write tha data of schema what we have created.
   const createdUser= await userModel.create({
@@ -17,9 +37,43 @@ router.get('/create',async function(req,res){
 
 })
 
+
 router.get("/allusers",async function(req,res){
-  const allUsers=await userModel.find() //.find() => we will get all users and it is also aynchronous 
+  const allUsers=await userModel.find() //.find() => we will get all users from collection and now everything related to userModel is asynchronous
+  res.send(allUsers) 
 })
+router.get("/allusers",async function(req,res){
+  const allUsers=await userModel.findOne({username:"saqib"}) //.findOne() => it will give anyone user's data when we will give instruction in the brackets({Objects}).If that user is not present then it will give null
+  res.send(allUsers) 
+})
+router.get("/delete",async function(req,res){
+  const deletedUser=await userModel.findOneAndDelete({username:"saqib123"}) //.findOneAndDelete() => it will give anyone user's data when we will give instruction in the brackets({Objects}) and it will delete by itself
+  res.send(deletedUser) 
+})
+
+
+router.get('/cookies',function(req,res){
+  res.cookie("saqib",20) //res.cookie() method is used to set cookies in the HTTP response that the server sends back to the client.res=> sending some data to browser from server
+
+  //cookie(name,value)
+
+  res.send("Hello from khan")
+ })
+
+ router.get('/checkCookies',function(req,res){
+  console.log(req.cookies); // here cookies. req=> as we want cookies from browser and into the server
+  res.send("cookies sent")
+
+  console.log(req.cookies.saqib); // only 20
+  
+ })
+
+ router.get('/deletecookies',function(req,res){
+  res.clearCookie("saqib") 
+  //clearCookie(nameofthecookie) 
+
+  res.send("Cookie deleted")
+ })
 
 //this is real one
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/olx")
-// .then(()=>{
-//   console.log("db connected");
+mongoose.connect("mongodb://127.0.0.1:27017/myappstorage")
+.then(()=>{
+  console.log("db connected");
   
-// })
-// .catch((error)=>{
-//   console.log(error);
+})
+.catch((error)=>{
+  console.log(error);
   
-// })//it says node plz connect ho through mongodb se....kyse 127.0.0.1 mah server chl rha h aur uska port h 27017
+})//it says node plz connect ho through mongodb se....kyse 127.0.0.1 mah server chl rha h aur uska port h 27017
 
 
 //schema created
-const userschema =  mongoose.Schema({
+const userschema = mongoose.Schema({
   username: String,
   name: String,
   age: Number
